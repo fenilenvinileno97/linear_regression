b6e41144266a18dd5c995b08076d981fa16fafb5
Marlon Marin <marlonmariin97@outlook.com>
2024-05-30 11:08:43 -0500

    (build) utils/linmodules.py was modified with plot_matrix_2x2() function able to plot 2x2 matrices. No changes in 2-prediction2D.ipynb

--- utils/linmodules.py
+++ utils/linmodules.py
@@ -18,6 +18,29 @@ def plot_vector(vecs, cols):
             angles="xy",
             color=cols[i]
         )
+        
+def plot_matrix_2x2(matrix, col_vector=["blue", "red"]):
+    """This function helps to plot a square matrix of 2x2 dimension."""
+    plt.axhline(y=0, color="gray", zorder=0)
+    plt.axvline(x=0, color="gray", zorder=0)
+    
+    # Unit trigonometric circle
+    x = np.linspace(-1, 1, 10000)
+    y = np.sqrt(1-(x**2))
+    
+    # Matrix transformating trigonometric circle
+    v1 = matrix[0,0]*x + matrix[0,1]*y
+    v2 = matrix[1,0]*x + matrix[1,1]*y
+    v1_neg = matrix[0,0]*x - matrix[0,1]*y
+    v2_neg = matrix[1,0]*x - matrix[1,1]*y
+    
+    # Plotting vectors
+    u = [matrix[0,0], matrix[0,1]]
+    v = [matrix[1,0], matrix[1,1]]
+    
+    plot_vector(vecs=[u,v], cols=[col_vector[0], col_vector[1]])
+    plt.plot(v1, v2, color="green", alpha=0.7)
+    plt.plot(v1_neg, v2_neg, color="green", alpha=0.7)
 
 def run():
     pass
